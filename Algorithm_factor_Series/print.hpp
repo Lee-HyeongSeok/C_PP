39097451d16fe96a74f3b47ae31503c1dd998e17
leehyeongseck <kea7109@naver.com>
2020-03-16 02:20:14 +0900

    print header

---
+++ Algorithm_factor_Series/print.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+template <typename T>
+inline void PRINT_ELEMENTS(const T& coll, const std::string& optstr = "") {
+	std::cout << optstr;
+	for (const auto& elem : coll) {
+		std::cout << elem << ' ';
+	}
+	std::cout << std::endl;
+}
